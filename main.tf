4cd0e78bb1ee629c1192ffa5c327cfbcd3513d59
MelissaMelendez15 <MelissaMelendez15@gmail.com>
2026-01-31 14:23:12 +0100

    feat: agregar nsg y reglas de entrada para ssh y jenkins

--- main.tf
+++ main.tf
@@ -1,18 +1,55 @@
 resource "azurerm_resource_group" "rg" {
-    name = "rg-jenkins-security-gate"
-    location = var.location
+  name     = "rg-jenkins-security-gate"
+  location = var.location
 }
 
 resource "azurerm_virtual_network" "vnet" {
-    name = "vnet-jenkins-security-gate"
-    address_space = ["10.10.0.0/16"]
-    location = azurerm_resource_group.rg.location
-    resource_group_name = azurerm_resource_group.rg.name
+  name                = "vnet-jenkins-security-gate"
+  address_space       = ["10.10.0.0/16"]
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
 }
 
 resource "azurerm_subnet" "subnet" {
-    name = "subnet-jenkins"
-    resource_group_name = azurerm_resource_group.rg.name
-    virtual_network_name = azurerm_virtual_network.vnet.name
-    address_prefixes = ["10.10.1.0/24"]
+  name                 = "subnet-jenkins"
+  resource_group_name  = azurerm_resource_group.rg.name
+  virtual_network_name = azurerm_virtual_network.vnet.name
+  address_prefixes     = ["10.10.1.0/24"]
 }
+
+resource "azurerm_network_security_group" "nsg" {
+  name                = "nsg-jenkins-security-gate"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+}
+
+resource "azurerm_network_security_rule" "allow_ssh_myip" {
+  name                        = "Allow-SSH-MyIP"
+  priority                    = 100
+  direction                   = "Inbound"
+  access                      = "Allow"
+  protocol                    = "Tcp"
+  source_port_range           = "*"
+  destination_port_range      = "22"
+  source_address_prefix       = var.my_ip
+  destination_address_prefix  = "*"
+  resource_group_name         = azurerm_resource_group.rg.name
+  network_security_group_name = azurerm_network_security_group.nsg.name
+}
+
+resource "azurerm_network_security_rule" "allow_jenkins_myip" {
+  name                        = "Allow-jenkins-MyIP"
+  priority                    = 110
+  direction                   = "Inbound"
+  access                      = "Allow"
+  protocol                    = "Tcp"
+  source_port_range           = "*"
+  destination_port_range      = "8080"
+  source_address_prefix       = var.my_ip
+  destination_address_prefix  = "*"
+  resource_group_name         = azurerm_resource_group.rg.name
+  network_security_group_name = azurerm_network_security_group.nsg.name
+}
+
+
+
--- variables.tf
+++ variables.tf
@@ -1,15 +1,15 @@
 variable "location" {
-    description = "Azure region"
-    type = string
-    default = "westeurope"
+  description = "Azure region"
+  type        = string
+  default     = "westeurope"
 }
 
 variable "my_ip" {
-    description = "IP pública desde la que se permitirá el acceso (formato CIDR)"
-    type = string
+  description = "IP pública desde la que se permitirá el acceso (formato CIDR)"
+  type        = string
 }
 
 variable "ssh_public_key_path" {
-    description = "Ruta a la clave pública SSH"
-    type = string
+  description = "Ruta a la clave pública SSH"
+  type        = string
 }

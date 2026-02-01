e9fe6f8875e8ae7c1a66bc712bbaf89c814f3dc5
MelissaMelendez15 <MelissaMelendez15@gmail.com>
2026-02-01 14:48:49 +0100

    refactor: Simplificar la estructura de las variables de terraform

--- variables.tf
+++ variables.tf
@@ -13,3 +13,16 @@ variable "ssh_public_key_path" {
   description = "Ruta a la clave pública SSH"
   type        = string
 }
+
+variable "vm_size" {
+  description = "Tamaño de la VM"
+  type        = string
+  default     = "Standard_B1s"
+}
+
+variable "admin_username" {
+  description = "Usuario administrador en la VM"
+  type        = string
+  default     = "azureuser"
+}
+

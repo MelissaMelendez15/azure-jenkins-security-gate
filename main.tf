f32e712831c13cedb425ee9d15b544520b3df242
MelissaMelendez15 <MelissaMelendez15@gmail.com>
2026-02-03 20:36:15 +0100

    chore: probar el webhook después de abrir 8080

--- main.tf
+++ main.tf
@@ -31,7 +31,7 @@ resource "azurerm_network_security_rule" "allow_ssh_myip" {
   protocol                    = "Tcp"
   source_port_range           = "*"
   destination_port_range      = "22"
-  source_address_prefix       = var.my_ip
+  source_address_prefix       = "*" //var.my_ip
   destination_address_prefix  = "*"
   resource_group_name         = azurerm_resource_group.rg.name
   network_security_group_name = azurerm_network_security_group.nsg.name

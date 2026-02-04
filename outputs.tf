792020130b45522401eb101e2170df6dc9f810aa
MelissaMelendez15 <MelissaMelendez15@gmail.com>
2026-02-04 20:04:36 +0100

    chore: elimina Terraform duplicado de la raíz

--- outputs.tf
+++
@@ -1,9 +0,0 @@
-output "public_ip" {
-  value = azurerm_public_ip.pip.ip_address
-}
-
-output "ssh_command" {
-  value = "ssh ${var.admin_username}@${azurerm_public_ip.pip.ip_address}"
-}
-
-
--- terraform.tfvars
+++
@@ -1,2 +0,0 @@
-my_ip               = "79.146.26.7/32"
-ssh_public_key_path = "/home/vant/.ssh/id_rsa.pub"
